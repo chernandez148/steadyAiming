e59bd19b38473cc8d8c8f434fc3bcc7d00f2376d
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2020-06-15 17:20:50 -0700

    Add files via upload

---
+++ steady aiming/js/sslideshow.js
@@ -0,0 +1,34 @@
+<!-- Tell a Story-->
+<div class="stories">
+	<h3 class="h3Story">Tell a Story | Blog</h3>
+
+<!-- SlideSow-->
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext"><h2>Steady Aiming :: In the City</h2></div>
+	<img class="slide" src="img/city.jpg" alt="city">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext"><h2>Steady Aiming :: Beach Bound 2017</h2></div>
+  <img class="slide" src="img/palms.jpg" alt="city">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext"><h2>Steady Aiming :: Cali's Boroughs</h2></div>
+  <img class="slide" src="img/alley.jpg" alt="city">
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+<script src="js/transition.js"></script>
+
+<br>
+<hr>
